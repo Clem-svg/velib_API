a5340a1c1df0435a0a7173b9fb1b9ac9fd87d886
Clementine <clementine.fouque@gmail.com>
2021-01-20 16:23:25 +0100

    correcting interval

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const fetchInfoVelib = () => {
   const url ='https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=100&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes'
     fetch(url).then((response) =>
       response.json().then((data) => {
+        selector.innerHTML =""
         velibInfo(data);
+
       ;}))
       .catch((error) => console.error('error:', error));
 
@@ -13,13 +15,12 @@ const fetchInfoVelib = () => {
 
 const velibInfo = (data) => {
   const dataArray = data.records
-      for (let i = 0; i < dataArray.length; i++) {
+      for (let i = 0; i < 5; i++) {
         let name = dataArray[i].fields.name
         let numberElectricVelibs = dataArray[i].fields.ebike
         let numberClassicalVelibs = dataArray[i].fields.mechanical
         let numberOfAvailableSpots = dataArray[i].fields.numdocksavailable
 
-        const selector = document.getElementById('selector')
         showVelibStation(selector, name, numberClassicalVelibs, numberElectricVelibs, numberOfAvailableSpots)
 
       }
@@ -28,14 +29,17 @@ const velibInfo = (data) => {
 
 const showVelibStation = (selector, name, numberClassicalVelibs, numberElectricVelibs, numberOfAvailableSpots) => {
   selector.innerHTML += `
-    <div class="card border-primary mb-3" style="max-width: 20rem;">
-          <h2>Station : ${name}</h2>
-          <p>${numberClassicalVelibs} classical Velibs</p>
-          <p>${numberElectricVelibs} electric Velibs</p>
-          <p>${numberOfAvailableSpots} electric Velibs</p>
+    <div class="card border mb-3 mx-3" style="width: 20rem;">
+          <h3 class="card-header pt-2 px-2">Station : ${name}</h3>
+          <p class="pt-2 px-2">${numberClassicalVelibs} classical Velibs</p>
+          <p class="pt-2 px-2">${numberElectricVelibs} electric Velibs</p>
+          <p class="pt-2 px-2">${numberOfAvailableSpots} electric Velibs</p>
       </div>
   `
+
 }
 
+const selector = document.getElementById('selector')
 
+setInterval(fetchInfoVelib, 3000);
 setInterval(fetchInfoVelib, 3000);
